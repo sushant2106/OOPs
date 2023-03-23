b95e9ff217b647dbd763cb41ab7c6c84185179ca
sushant2106 <90310743+sushant2106@users.noreply.github.com>
2023-03-23 14:39:15 +0530

    Create constructor.cpp

---
+++ constructor.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+class A{
+    public:
+    A()
+    {
+        cout<<"I am A\n";
+    }
+    A(int x)
+    {
+        cout<<"I am "<<x;
+    }
+    A(string x)
+    {
+        cout<<x;
+    }
+};
+//class B:public A{
+//     public:
+//     B()
+//     {
+//          cout<<"I am B\n";
+//     }
+// };
+int main()
+{
+    A obj(10);
+    A obj1("ram");
+}
